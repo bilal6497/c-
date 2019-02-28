2a12f884b99353162f3c700315082b6b00486e7e
bilal6497 <47634652+bilal6497@users.noreply.github.com>
2019-02-28 14:28:30 +0300

    Add files via upload

---
+++ yas hesaplama programı.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+    int main() {
+    	
+    	int x;
+    	int y;
+    	
+    	cout<<"Lutfen dogum tarihinizi girin:";
+    	
+    	cin>>x;
+    	
+    	y = 2019-x;
+    	cout<<"Su anki yasiniz:"<<y;
+    	
+    	
+    	return 0;
+	}
