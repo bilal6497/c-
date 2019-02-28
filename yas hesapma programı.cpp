46fcbfd009d561f82603a82072551e8eb235b0f1
bilal6497 <47634652+bilal6497@users.noreply.github.com>
2019-02-28 14:27:19 +0300

    Add files via upload

---
+++ yas hesapma programı.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;	
+
+
+ int main() {
+ 	
+ 	int yas;
+ 	
+ 	cout<<"yasınızı gırınız:";	
+ 	cin>>yas;
+ 	cout<<"Su ankı Yasınız:"<<yas;
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	
+ 	return 0;
+ }
---
+++ yazı bastırma.cpp
@@ -0,0 +1,10 @@
+// ilk program 26.02.2019
+ 
+#include<iostream>
+using namespace std;
+ 
+int main ()
+{
+	cout << "merhaba C++";
+	return 0;
+}
